48e9e1f100f1fb0c1c081a1f7b5f07413206d70f
Sundareswar Pullela <110754869+sundareswarpullela@users.noreply.github.com>
2024-10-21 19:48:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -103,3 +103,22 @@ def ask_question():
         st.session_state.messages.append({"role": "user", "content": user_question})
         response = get_response_from_chatbot(user_question)
         st.session_state.messages.append({"role": "bot", "content": response})
+
+ask_question()
+# if "messages" not in st.session_state:
+#     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+# for msg in st.session_state.messages:
+#     st.chat_message(msg["role"]).write(msg["content"])
+
+# if prompt := st.chat_input():
+#     if not openai_api_key:
+#         st.info("Please add your OpenAI API key to continue.")
+#         st.stop()
+
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+#     st.chat_message("user").write(prompt)
+#     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+#     msg = response.choices[0].message.content
+#     st.session_state.messages.append({"role": "assistant", "content": msg})
+#     st.chat_message("assistant").write(msg)
